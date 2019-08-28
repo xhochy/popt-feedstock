4235b126915a26a004854ca15220cd8a5e565a65
Chris Burr <chrisburr@users.noreply.github.com>
2019-08-28 23:41:39 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,8 @@
 
 set -e -o pipefail
 
+curl http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD > config.guess
+curl http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD > config.sub
 ./configure --prefix=$PREFIX
 make
 make check
